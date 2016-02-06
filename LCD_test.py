781345b8f92f2897de1f2d7b2693451943aa797f
Guilherme Boaviagem Ribeiro <gboaviagem@GBRs.local>
2016-02-06 18:10:17 -0300

    updated RIPO and LCD_test

--- LCD_test.py
+++ LCD_test.py
@@ -1,177 +1,23 @@
-#!/usr/bin/python
+from RIPO import *
 
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
+lcd_pins_setup()
+lcd_init()
 
-#import
-import RPi.GPIO as GPIO
-# To install RPi.GPIO:
-# >> sudo apt-get update
-# >> sudo apt-get install rpi.gpio
-import time
+# Push buttons constants (BCM GPIO)
+PB_1 = 16
+PB_2 = 12
 
-# -----------------------------
-# Define GPIO to LCD mapping
+PB1 = Button(pin=PB_1, pull_up=True, bounce_time=None)
+PB2 = Button(pin=PB_2, pull_up=True, bounce_time=None)
 
-# BCM GPIO
-# LCD_RS = 7
-# LCD_E  = 8
-# LCD_D4 = 25
-# LCD_D5 = 24
-# LCD_D6 = 23
-# LCD_D7 = 18
+if PB1.is_pressed and (not PB2.is_pressed):
+	lcd_string("Botao 01",LCD_LINE_1)
+	lcd_string("funcionando",LCD_LINE_1)
 
-# BOARD GPIO
-LCD_RS = 26
-LCD_E  = 24
-LCD_D4 = 22
-LCD_D5 = 18
-LCD_D6 = 16
-LCD_D7 = 12
-
-
-# Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
-
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
-
-
-def lcd_init():
-	# Initialise display
-	lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-	lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-	lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-	lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-	lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-	lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-	time.sleep(E_DELAY)
-
-def lcd_byte(bits, mode):
-	# Send byte to data pins
-	# bits = data
-	# mode = True  for character
-	#        False for command
-
-	GPIO.output(LCD_RS, mode) # RS
-
-	# High bits
-	GPIO.output(LCD_D4, False)
-	GPIO.output(LCD_D5, False)
-	GPIO.output(LCD_D6, False)
-	GPIO.output(LCD_D7, False)
-	if bits&0x10==0x10:
-		GPIO.output(LCD_D4, True)
-	if bits&0x20==0x20:
-		GPIO.output(LCD_D5, True)
-	if bits&0x40==0x40:
-		GPIO.output(LCD_D6, True)
-	if bits&0x80==0x80:
-		GPIO.output(LCD_D7, True)
-
-	# Toggle 'Enable' pin
-	lcd_toggle_enable()
-
-	# Low bits
-	GPIO.output(LCD_D4, False)
-	GPIO.output(LCD_D5, False)
-	GPIO.output(LCD_D6, False)
-	GPIO.output(LCD_D7, False)
-	if bits&0x01==0x01:
-		GPIO.output(LCD_D4, True)
-	if bits&0x02==0x02:
-		GPIO.output(LCD_D5, True)
-	if bits&0x04==0x04:
-		GPIO.output(LCD_D6, True)
-	if bits&0x08==0x08:
-		GPIO.output(LCD_D7, True)
-
-	# Toggle 'Enable' pin
-	lcd_toggle_enable()
-
-def lcd_toggle_enable():
-	# Toggle enable
-	time.sleep(E_DELAY)
-	GPIO.output(LCD_E, True)
-	time.sleep(E_PULSE)
-	GPIO.output(LCD_E, False)
-	time.sleep(E_DELAY)
-
-def lcd_string(message,line):
-	# Send string to display
-
-	message = message.ljust(LCD_WIDTH," ")
-
-	lcd_byte(line, LCD_CMD)
-
-	for i in range(LCD_WIDTH):
-		lcd_byte(ord(message[i]),LCD_CHR)
-
-# -------------------------------------------------------
-def main():
-	# Main program block
-
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)       # Use BOARD GPIO numbers
-	GPIO.setup(LCD_E, GPIO.OUT)  # E
-	GPIO.setup(LCD_RS, GPIO.OUT) # RS
-	GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-	GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-	GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-	GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-
-
-	# Initialise display
-	lcd_init()
-
-	for i in range(3):
-
-		# Send some test
-		lcd_string("Rasbperry Pi",LCD_LINE_1)
-		lcd_string("16x2 LCD Test",LCD_LINE_2)
-
-		time.sleep(3) # 3 second delay
-
-		# Send some text
-		lcd_string("1234567890123456",LCD_LINE_1)
-		lcd_string("abcdefghijklmnop",LCD_LINE_2)
-
-
-		time.sleep(3) # 3 second delay
-
-		# Send some text
-		lcd_string("RaspberryPi-spy",LCD_LINE_1)
-		lcd_string(".co.uk",LCD_LINE_2)
-
-		time.sleep(3)
-
-		# Send some text
-		lcd_string("Follow me on",LCD_LINE_1)
-		lcd_string("Twitter @RPiSpy",LCD_LINE_2)
-
-		time.sleep(3)
+if PB2.is_pressed and (not PB1.is_pressed):
+	lcd_string("Botao 02",LCD_LINE_1)
+	lcd_string("funcionando tbm",LCD_LINE_1)
 	
-	
-# EXECUTE
-main()
+if PB1.is_pressed and PB2.is_pressed:
+	lcd_string("Desligando",LCD_LINE_1)
+	lcd_string("Tchau!",LCD_LINE_1)
